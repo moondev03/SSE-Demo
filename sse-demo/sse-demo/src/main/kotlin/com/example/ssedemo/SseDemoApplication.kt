e6bca40469dbd19b6b34a44cae715bce8e455c3a
moondev03 <moondev03@gmail.com>
2025-09-15 14:54:24 +0900

    init: Spring 프로젝트 생성

---
+++ sse-demo/sse-demo/src/main/kotlin/com/example/ssedemo/SseDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ssedemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SseDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<SseDemoApplication>(*args)
+}
